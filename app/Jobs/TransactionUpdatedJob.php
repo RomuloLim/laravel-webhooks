67ae01792efb65b62a2722f6ef2b5f3933a9ba41
RomuloLim <romulo.lf123@gmail.com>
2022-10-07 18:02:30 -0300

    wip

---
+++ app/Jobs/TransactionUpdatedJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
+
+class TransactionUpdatedJob extends ProcessWebhookJob
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        logger('wello Webhook: '.Carbon::now()->toDateTimeString());
+    }
+}
